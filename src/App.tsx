e25eb53c5bd8cf7e0055fb03542ceb5a59a4c033
Brandyn Britton <Brandynbb96@gmail.com>
2023-02-02 12:10:50 +0800

    Progress on viewing calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import {useEffect, useState} from "react"
-import {ChakraProvider, theme,} from "@chakra-ui/react"
+import {ChakraProvider, Icon, IconButton, theme,} from "@chakra-ui/react"
 
 import "./main.css";
 import FrogImage from "./components/FrogImage";
@@ -8,11 +8,14 @@ import {auth} from "./FirebaseConfig";
 import SignIn from "./screens/SignIn";
 import ActivitySelection from "./screens/ActivitySelection";
 import {ColorModeSwitcher} from "./ColorModeSwitcher";
+import ViewCalendar from "./screens/ViewCalendar";
+import {AiTwotoneCalendar} from "react-icons/ai";
 
 
 export const App = () => {
 
     const [isSignedIn, setIsSignedIn] = useState(!!(auth.currentUser ?? true));
+    const [isViewingCalendar, setIsViewingCalendar] = useState(false);
 
 
     useEffect(() => {
@@ -21,16 +24,22 @@ export const App = () => {
         })
     }, [isSignedIn])
 
+    const ToggleCalendar = () => {
+        setIsViewingCalendar(!isViewingCalendar);
+    }
 
     return (
         <ChakraProvider theme={theme}>
             <ColorModeSwitcher/>
             <div className={"content-container"}>
-                {!isSignedIn ? <SignIn/> : <ActivitySelection/>}
+
+
+                {!isSignedIn ? <SignIn/> :isViewingCalendar ? <ViewCalendar/> : <ActivitySelection setShowCalendar={ToggleCalendar}/> }
 
             </div>
 
             <FrogImage/>
+            <IconButton style={{position: "absolute", right: "4", top: "4"}} aria-label={"View Calendar"} icon={<Icon as={AiTwotoneCalendar} />} onClick={ToggleCalendar} />
         </ChakraProvider>
     )
 }
---
+++ src/screens/ViewCalendar.tsx
@@ -0,0 +1,12 @@
+const ViewCalendar = () => {
+
+    return (
+        <iframe
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%237CB342&ctz=Australia%2FPerth&showNav=1&showPrint=0&showTabs=0&showCalendars=0&showTitle=1&showTz=0&title=Frog'n'Bee&src=ZmFtaWx5MTU3MDU4Nzk0MDI5MjA0OTY5NDNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4uYXVzdHJhbGlhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23A79B8E&color=%23009688"
+            style={{borderWidth: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+    )
+
+
+}
+
+export default ViewCalendar;

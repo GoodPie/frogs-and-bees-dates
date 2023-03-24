7edf677100434b8e3c022902fce1ae3c616fd276
Brandyn Britton <Brandynbb96@gmail.com>
2023-03-24 15:51:55 +0800

    update: merge and ready for notifications

--- src/screens/ViewCalendar.tsx
+++ src/screens/ViewCalendar.tsx
@@ -1,41 +1,69 @@
+import React, {useEffect, useState} from "react";
+import {collection, getDocs, orderBy, query, where} from "firebase/firestore";
+import {db} from "../FirebaseConfig";
+import ICalendarActivity from "../interfaces/ICalendarActivity";
+import CalendarEvent from "../components/CalendarEvent";
+import {Heading, HStack, Stack, VStack} from "@chakra-ui/react";
+import dayjs from "dayjs";
 
 const ViewCalendar = () => {
 
-    /**
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [events, setEvents] = useState<ICalendarActivity[]>([]);
-    const [currentDate, setCurrentDate] = useState(new Date())
+    const calendarRef = collection(db, "calendarEvents");
+    const [calendarEvents, setCalendarEvents] = useState([] as ICalendarActivity[]);
 
     useEffect(() => {
+        RefreshEvents();
 
-    }, [events]);
+    }, []);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const GoToNextDate = () => {
 
-        // Sort the events by the date, ascending
-        const sortedEvents = events.sort((a, b) => {
-           return new Date(a.date).getTime() - new Date(b.date).getTime();
-        });
+    const RefreshEvents = () => {
+        const startDate = dayjs().startOf("day").toDate().valueOf() / 1000;
 
-        // Find the next event
-        const nextEvent = sortedEvents.find(e => new Date(e.date).getTime() > currentDate.getTime());
+        let calendarQuery = query(calendarRef,
+            where("date", ">=", startDate),
+            orderBy("date"))
 
-        // If there is a next event, set the current date to the next event
-        if (nextEvent) {
-            setCurrentDate(new Date(nextEvent.date));
-        }
+        getDocs(calendarQuery)
+            .then((snapShot) => {
+                if (!snapShot.empty) {
+                    const events = [] as ICalendarActivity[];
+                    snapShot.docs.forEach((doc) => {
+
+                        const model = doc.data() as ICalendarActivity;
+                        if (model.done !== true) {
+                            model.id = doc.id;
+                            events.push(model)
+                        }
+
+                    });
+
+                    setCalendarEvents(events);
+                } else {
+
+                }
+
+            })
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const GoToPreviousDate = () => {
-
-    }
-    **/
 
     return (
-        <h1>Viewing Calendar</h1>
+        <VStack h={"50vh"} overflow={"auto"} p={10}>
 
+            <Stack spacing={4} justifyContent={"center"} flexWrap={"wrap"}>
+                {calendarEvents.map(ce => {
+                    return <CalendarEvent id={ce.id} activityName={ce.activityName} date={ce.date}
+                                          activityDesc={ce.activityDesc} onEventMarkedAsDone={RefreshEvents}/>
+                })}
+
+                {calendarEvents.length === 0 &&
+                    <Stack>
+                        <Heading as={"h3"}>No Upcoming Events 😔</Heading>
+
+                    </Stack>
+                }
+            </Stack>
+
+        </VStack>
     )
 
 

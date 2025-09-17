fd3723f430650a43fae2e6366ea0732fcc8e2359
Brandyn Britton <brandynbb96@gmail.com>
2025-09-17 09:03:34 +0800

    tests: added factories

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,10 +1,41 @@
-import { render as rtlRender } from "@testing-library/react"
-import { Provider } from '../src/components/ui/provider';
+import { render as rtlRender, RenderOptions, RenderResult } from "@testing-library/react"
+import { ReactElement } from "react"
+import { Provider } from '../src/components/ui/provider'
 
-export function render(ui: React.ReactNode) {
-  return rtlRender(<>{ui}</>, {
-    wrapper: (props: React.PropsWithChildren) => (
-      <Provider>{props.children}</Provider>
-    ),
-  })
+// Custom render options interface
+interface CustomRenderOptions extends Omit<RenderOptions, 'wrapper'> {
+  initialProps?: any
+  wrapper?: React.ComponentType<any>
 }
+
+// Custom render result interface
+interface CustomRenderResult extends RenderResult {
+  rerender: (ui: ReactElement) => void
+}
+
+// Enhanced render function with Chakra UI provider
+export function render(
+  ui: ReactElement,
+  options: CustomRenderOptions = {}
+): CustomRenderResult {
+  const { wrapper: Wrapper = Provider, ...renderOptions } = options
+
+  const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+    return <Wrapper>{children}</Wrapper>
+  }
+
+  const result = rtlRender(ui, {
+    wrapper: AllTheProviders,
+    ...renderOptions,
+  })
+
+  return {
+    ...result,
+    rerender: (newUi: ReactElement) =>
+      result.rerender(<AllTheProviders>{newUi}</AllTheProviders>),
+  }
+}
+
+// Re-export everything from testing library
+export * from "@testing-library/react"
+export { default as userEvent } from "@testing-library/user-event"

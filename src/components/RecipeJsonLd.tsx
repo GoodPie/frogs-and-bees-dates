58e068b13b946a4b5a88566247a2e3fe99255f28
Brandyn Britton <brandynbb96@gmail.com>
2025-10-26 20:23:24 +0800

    feat(recipes): Added JSON-LD format

---
+++ src/components/RecipeJsonLd.tsx
@@ -0,0 +1,71 @@
+import { Helmet } from 'react-helmet-async';
+import { IRecipe } from '@/interfaces/IRecipe';
+
+interface RecipeJsonLdProps {
+    recipe: IRecipe;
+}
+
+/**
+ * Component that generates JSON-LD structured data for recipes
+ * Following Google's Recipe structured data guidelines
+ * https://developers.google.com/search/docs/appearance/structured-data/recipe
+ */
+export const RecipeJsonLd = ({ recipe }: RecipeJsonLdProps) => {
+    const structuredData = {
+        '@context': 'https://schema.org/',
+        '@type': 'Recipe',
+        name: recipe.name,
+        image: Array.isArray(recipe.image) ? recipe.image : [recipe.image],
+        description: recipe.description,
+        author: recipe.author ? {
+            '@type': 'Person',
+            name: recipe.author
+        } : undefined,
+        datePublished: recipe.datePublished?.toISOString(),
+        prepTime: recipe.prepTime,
+        cookTime: recipe.cookTime,
+        totalTime: recipe.totalTime,
+        recipeYield: recipe.recipeYield,
+        recipeCategory: recipe.recipeCategory,
+        recipeCuisine: recipe.recipeCuisine,
+        keywords: recipe.keywords?.join(', '),
+        recipeIngredient: recipe.recipeIngredient,
+        recipeInstructions: recipe.recipeInstructions.map((instruction, index) => ({
+            '@type': 'HowToStep',
+            position: index + 1,
+            text: instruction
+        })),
+        nutrition: recipe.nutrition ? {
+            '@type': 'NutritionInformation',
+            calories: recipe.nutrition.calories,
+            carbohydrateContent: recipe.nutrition.carbohydrateContent,
+            proteinContent: recipe.nutrition.proteinContent,
+            fatContent: recipe.nutrition.fatContent,
+            saturatedFatContent: recipe.nutrition.saturatedFatContent,
+            unsaturatedFatContent: recipe.nutrition.unsaturatedFatContent,
+            transFatContent: recipe.nutrition.transFatContent,
+            cholesterolContent: recipe.nutrition.cholesterolContent,
+            sodiumContent: recipe.nutrition.sodiumContent,
+            fiberContent: recipe.nutrition.fiberContent,
+            sugarContent: recipe.nutrition.sugarContent,
+            servingSize: recipe.nutrition.servingSize,
+        } : undefined,
+        aggregateRating: recipe.aggregateRating ? {
+            '@type': 'AggregateRating',
+            ratingValue: recipe.aggregateRating.ratingValue,
+            ratingCount: recipe.aggregateRating.ratingCount,
+        } : undefined,
+        suitableForDiet: recipe.suitableForDiet?.map(diet => `https://schema.org/${diet}`),
+    };
+
+    // Remove undefined fields
+    const cleanedData = JSON.parse(JSON.stringify(structuredData));
+
+    return (
+        <Helmet>
+            <script type="application/ld+json">
+                {JSON.stringify(cleanedData, null, 2)}
+            </script>
+        </Helmet>
+    );
+};

601e14f01ebbafac52a05d97fe8efb8b171518f4
Brandyn Britton <brandynbb96@gmail.com>
2025-10-26 20:25:29 +0800

    feat(recipes): added react-helm-async Used to support JSON-LD

--- src/components/RecipeJsonLd.tsx
+++ src/components/RecipeJsonLd.tsx
@@ -1,5 +1,5 @@
-import { Helmet } from 'react-helmet-async';
-import { IRecipe } from '@/interfaces/IRecipe';
+import { useEffect } from 'react';
+import type {IRecipe} from '@/interfaces/IRecipe';
 
 interface RecipeJsonLdProps {
     recipe: IRecipe;
@@ -11,61 +11,72 @@ interface RecipeJsonLdProps {
  * https://developers.google.com/search/docs/appearance/structured-data/recipe
  */
 export const RecipeJsonLd = ({ recipe }: RecipeJsonLdProps) => {
-    const structuredData = {
-        '@context': 'https://schema.org/',
-        '@type': 'Recipe',
-        name: recipe.name,
-        image: Array.isArray(recipe.image) ? recipe.image : [recipe.image],
-        description: recipe.description,
-        author: recipe.author ? {
-            '@type': 'Person',
-            name: recipe.author
-        } : undefined,
-        datePublished: recipe.datePublished?.toISOString(),
-        prepTime: recipe.prepTime,
-        cookTime: recipe.cookTime,
-        totalTime: recipe.totalTime,
-        recipeYield: recipe.recipeYield,
-        recipeCategory: recipe.recipeCategory,
-        recipeCuisine: recipe.recipeCuisine,
-        keywords: recipe.keywords?.join(', '),
-        recipeIngredient: recipe.recipeIngredient,
-        recipeInstructions: recipe.recipeInstructions.map((instruction, index) => ({
-            '@type': 'HowToStep',
-            position: index + 1,
-            text: instruction
-        })),
-        nutrition: recipe.nutrition ? {
-            '@type': 'NutritionInformation',
-            calories: recipe.nutrition.calories,
-            carbohydrateContent: recipe.nutrition.carbohydrateContent,
-            proteinContent: recipe.nutrition.proteinContent,
-            fatContent: recipe.nutrition.fatContent,
-            saturatedFatContent: recipe.nutrition.saturatedFatContent,
-            unsaturatedFatContent: recipe.nutrition.unsaturatedFatContent,
-            transFatContent: recipe.nutrition.transFatContent,
-            cholesterolContent: recipe.nutrition.cholesterolContent,
-            sodiumContent: recipe.nutrition.sodiumContent,
-            fiberContent: recipe.nutrition.fiberContent,
-            sugarContent: recipe.nutrition.sugarContent,
-            servingSize: recipe.nutrition.servingSize,
-        } : undefined,
-        aggregateRating: recipe.aggregateRating ? {
-            '@type': 'AggregateRating',
-            ratingValue: recipe.aggregateRating.ratingValue,
-            ratingCount: recipe.aggregateRating.ratingCount,
-        } : undefined,
-        suitableForDiet: recipe.suitableForDiet?.map(diet => `https://schema.org/${diet}`),
-    };
+    useEffect(() => {
+        const structuredData = {
+            '@context': 'https://schema.org/',
+            '@type': 'Recipe',
+            name: recipe.name,
+            image: Array.isArray(recipe.image) ? recipe.image : [recipe.image],
+            description: recipe.description,
+            author: recipe.author ? {
+                '@type': 'Person',
+                name: recipe.author
+            } : undefined,
+            datePublished: recipe.datePublished?.toISOString(),
+            prepTime: recipe.prepTime,
+            cookTime: recipe.cookTime,
+            totalTime: recipe.totalTime,
+            recipeYield: recipe.recipeYield,
+            recipeCategory: recipe.recipeCategory,
+            recipeCuisine: recipe.recipeCuisine,
+            keywords: recipe.keywords?.join(', '),
+            recipeIngredient: recipe.recipeIngredient,
+            recipeInstructions: recipe.recipeInstructions.map((instruction, index) => ({
+                '@type': 'HowToStep',
+                position: index + 1,
+                text: instruction
+            })),
+            nutrition: recipe.nutrition ? {
+                '@type': 'NutritionInformation',
+                calories: recipe.nutrition.calories,
+                carbohydrateContent: recipe.nutrition.carbohydrateContent,
+                proteinContent: recipe.nutrition.proteinContent,
+                fatContent: recipe.nutrition.fatContent,
+                saturatedFatContent: recipe.nutrition.saturatedFatContent,
+                unsaturatedFatContent: recipe.nutrition.unsaturatedFatContent,
+                transFatContent: recipe.nutrition.transFatContent,
+                cholesterolContent: recipe.nutrition.cholesterolContent,
+                sodiumContent: recipe.nutrition.sodiumContent,
+                fiberContent: recipe.nutrition.fiberContent,
+                sugarContent: recipe.nutrition.sugarContent,
+                servingSize: recipe.nutrition.servingSize,
+            } : undefined,
+            aggregateRating: recipe.aggregateRating ? {
+                '@type': 'AggregateRating',
+                ratingValue: recipe.aggregateRating.ratingValue,
+                ratingCount: recipe.aggregateRating.ratingCount,
+            } : undefined,
+            suitableForDiet: recipe.suitableForDiet?.map(diet => `https://schema.org/${diet}`),
+        };
 
-    // Remove undefined fields
-    const cleanedData = JSON.parse(JSON.stringify(structuredData));
+        // Remove undefined fields
+        const cleanedData = JSON.parse(JSON.stringify(structuredData));
 
-    return (
-        <Helmet>
-            <script type="application/ld+json">
-                {JSON.stringify(cleanedData, null, 2)}
-            </script>
-        </Helmet>
-    );
+        // Create and inject script tag
+        const script = document.createElement('script');
+        script.type = 'application/ld+json';
+        script.text = JSON.stringify(cleanedData, null, 2);
+        script.id = 'recipe-structured-data';
+        document.head.appendChild(script);
+
+        // Cleanup on unmount
+        return () => {
+            const existingScript = document.getElementById('recipe-structured-data');
+            if (existingScript) {
+                document.head.removeChild(existingScript);
+            }
+        };
+    }, [recipe]);
+
+    return null;
 };
